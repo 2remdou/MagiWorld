6890906399cf6cb745e5d65e014d03900dd19096
2remdou <2remdou@gmail.com>
2019-08-14 06:15:19 +0000

    - creation des classes Personnage et Guerrier

---
+++ src/com/oc/Guerrier.java
@@ -0,0 +1,24 @@
+package com.oc;
+
+public class Guerrier extends Personnage {
+    @Override
+    public String nomAttaqueBasique() {
+        return "Coup d'Épée";
+    }
+
+    @Override
+    public String nomAttaqueSpeciale() {
+        return "Coup de Rage";
+    }
+
+    @Override
+    public void attaqueBasique(Personnage personnage) {
+
+    }
+
+    @Override
+    public void attaqueSpeciale(Personnage personnage) {
+
+    }
+
+}
--- src/com/oc/Personnage.java
+++ src/com/oc/Personnage.java
@@ -1,4 +1,46 @@
 package com.oc;
 
 public abstract class Personnage {
+    private int niveau;
+    private int force;
+    private int agilite;
+    private int intelligence;
+
+    public abstract String nomAttaqueBasique();
+    public abstract String nomAttaqueSpeciale();
+    public abstract void attaqueBasique(Personnage personnage);
+    public abstract void attaqueSpeciale(Personnage personnage);
+
+    public int getNiveau() {
+        return niveau;
+    }
+
+    public void setNiveau(int niveau) {
+        this.niveau = niveau;
+    }
+
+    public int getForce() {
+        return force;
+    }
+
+    public void setForce(int force) {
+        this.force = force;
+    }
+
+    public int getAgilite() {
+        return agilite;
+    }
+
+    public void setAgilite(int agilite) {
+        this.agilite = agilite;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
 }
